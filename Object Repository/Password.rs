5bbf546c21aa46f1af231480726a551331c400b7
RakshithNagThota <RakshithNagThota@RakshithNag-Thota.home>
2023-06-22 12:06:33 -0400

    demo practice

---
+++ Object Repository/Password.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Password</name>
+   <tag></tag>
+   <elementGuidId>6bdd4027-292e-4f41-8af9-bba1bb3c0f5a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='password']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
